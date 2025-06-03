0cfff69912092af15822d3ac0c82ae7e716f6707
lizweaver <elizabeth.weaver@amd.com>
2025-06-03 10:27:04 -0700

    moving template code to core fork repo

--- tests/validators.py
+++ tests/validators.py
@@ -560,3 +560,29 @@ def validate_rook_ceph_integration():
 
     finally:
         kubectl("delete -f {}".format(manifest))
+
+def validate_amd():
+    """
+    Validate AMD by running a simple test pod that runs 'amd-smi' and checks output.
+    """
+
+    if platform.machine() != "x86_64":
+        print("GPU tests are only relevant on x86 architectures")
+        return
+
+    namespace = "default"
+    test_pod_name = "amd-smi"
+    smi_manifest = TEMPLATES / "amd-smi.yaml"
+
+    existing_pods = kubectl(f"get po {test_pod_name} -n {namespace}")
+    if test_pod_name in existing_pods:
+        kubectl(f"delete po {test_pod_name} -n {namespace}")
+        time.sleep(10)
+
+    kubectl(f"apply -f {smi_manifest}")
+    wait_for_pod_state(test_pod_name, namespace, "Succeeded", timeout_insec=600)
+
+    logs = kubectl(f"logs pod/{test_pod_name} -n {namespace}")
+
+    passed = "AMDSMI Tool" in logs and "ROCm version" in logs
+    assert passed
